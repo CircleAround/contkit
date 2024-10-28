9d94735479f709ee6d5aa9b62559753156199fc8
hiokky <hiokky.work@gmail.com>
2024-10-28 14:35:13 +0900

    Selectコンポーネントを追加

---
+++ site/src/components/Form/FormSelect/Select.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ComponentProps, forwardRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+import {
+  Select as UISelect,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+type SelectProps = ComponentProps<typeof UISelect> & {
+  className?: string;
+  selects?: { value: string, name: string }[];
+}
+
+const Select = forwardRef<HTMLButtonElement, SelectProps>(({ selects = [], className, ...others }, ref) => {
+  const baseCn = 'border-zinc-300 h-11 text-zinc-900 placeholder:text-zinc-400 focus:outline-none focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-0';
+
+    return (
+      <UISelect>
+        <SelectTrigger
+          className={twMerge(baseCn, className)}
+          {...others}
+          ref={ref}
+        >
+          <SelectValue placeholder="選択してください" />
+        </SelectTrigger>
+        <SelectContent className='border-zinc-300 text-zinc-900'>
+          {selects.map((select, index) => (
+            <SelectItem
+              key={index}
+              value={select.value}
+            >
+              {select.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </UISelect>
+    )
+  },
+)
+
+Select.displayName = 'Select'
+
+export { Select }
