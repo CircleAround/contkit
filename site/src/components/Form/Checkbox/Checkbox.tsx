790ba97571f7cbbe155d1f8ff9295e5bb34a5094
hiokky <hiokky.work@gmail.com>
2024-10-29 17:40:06 +0900

    スタイルの変更

--- site/src/components/Form/Checkbox/Checkbox.tsx
+++ site/src/components/Form/Checkbox/Checkbox.tsx
@@ -4,17 +4,17 @@ import { twMerge } from 'tailwind-merge';
 import { Checkbox as UICheckbox } from "@/components/ui/checkbox"
 
 type CheckboxProps = ComponentProps<typeof UICheckbox> & {
-  checkboxCaption?: string;
+  caption?: string;
 }
 
-const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(({ id, className, checkboxCaption, ...others }, ref) => {
-  const baseCn = 'border-zinc-400 w-5 h-5 data-[state=checked]:bg-green-500 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:ring-offset-0 focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-0';
+const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(({ className, caption, ...others }, ref) => {
+  const baseCn = 'border-zinc-400 w-5 h-5 data-[state=checked]:bg-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:ring-offset-0 focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-0';
 
     return (
       <label className="flex items-center space-x-2 cursor-pointer">
         <UICheckbox ref={ref} className={twMerge(baseCn, className)} {...others} />
-        {checkboxCaption &&
-          <span className="text-zinc-900 text-sm cursor-pointer">{checkboxCaption}</span>
+        {caption &&
+          <span className="text-zinc-900 text-sm">{caption}</span>
         }
       </label>
     )
