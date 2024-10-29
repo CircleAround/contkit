c9d0ff3f5704bb08d63e26c03149f70c73daf00f
hiokky <hiokky.work@gmail.com>
2024-10-29 16:55:17 +0900

    スタイルの修正

--- site/src/components/Form/Checkbox/Checkbox.tsx
+++ site/src/components/Form/Checkbox/Checkbox.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { ComponentProps, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { Checkbox as UICheckbox } from "@/components/ui/checkbox"
-import { Label } from '@/components/ui/label';
 
 type CheckboxProps = ComponentProps<typeof UICheckbox> & {
   checkboxCaption?: string;
 }
 
 const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(({ id, className, checkboxCaption, ...others }, ref) => {
-  const baseCn = 'border-zinc-400';
+  const baseCn = 'border-zinc-400 w-5 h-5 data-[state=checked]:bg-green-500 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:ring-offset-0 focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-0';
 
     return (
       <label className="flex items-center space-x-2 cursor-pointer">
-        <UICheckbox id={id} ref={ref} className={twMerge(baseCn, className)} {...others} />
+        <UICheckbox ref={ref} className={twMerge(baseCn, className)} {...others} />
         {checkboxCaption &&
           <span className="block text-zinc-900 text-sm cursor-pointer">{checkboxCaption}</span>
         }
@@ -22,26 +21,6 @@ const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(({ id, className, 
   },
 )
 
-type CheckboxListProps = CheckboxProps & {
-  checkboxCaption?: string;
-  checkboxitems: { caption: string }[];
-}
-
-const CheckboxList = forwardRef<HTMLButtonElement, CheckboxListProps>(({ checkboxitems = [], className, checkboxCaption, ...others }, ref) => {
-
-    return (
-      <ul className="space-y-1">
-        {checkboxitems.map((checkboxitem, index) => (
-          <li key={index} className="">
-            <Checkbox checkboxCaption={checkboxitem.caption}/>
-          </li>
-        ))}
-      </ul>
-    )
-  },
-)
-
 Checkbox.displayName = 'Checkbox'
-CheckboxList.displayName = 'CheckboxList'
 
-export { Checkbox, CheckboxList }
+export { Checkbox }
