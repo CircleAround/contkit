340b46195105283073ef78af2780e8741d441d9b
hiokky <hiokky.work@gmail.com>
2024-10-30 14:38:07 +0900

    FormMessageの通常時とエラー時のスタイルの変化を状態管理

--- site/src/components/Form/Message.tsx
+++ site/src/components/Form/Message.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import { ComponentProps, forwardRef } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
 import { twMerge } from 'tailwind-merge';
 import { FormMessage as UIFormMessage } from '@/components/ui/form';
 
+const FormMessageVariants = cva(
+  '',
+  {
+    variants: {
+      state: {
+        default: 'text-zinc-900',
+        danger: 'text-red-500',
+      },
+    },
+    defaultVariants: {
+      state: 'default',
+    },
+  }
+);
 
-type FormMessageProps = ComponentProps<typeof UIFormMessage>
+type FormMessageProps = ComponentProps<typeof UIFormMessage> & VariantProps<typeof FormMessageVariants>
 
-const FormMessage = forwardRef<HTMLParagraphElement, FormMessageProps>(({ className, children, ...others }, ref) => {
-    const baseCn = 'text-red-500'
+const FormMessage = forwardRef<HTMLParagraphElement, FormMessageProps>(({ state, className, children, ...others }, ref) => {
 
     return (
       <UIFormMessage
-        className={twMerge(baseCn, className)}
+        className={twMerge(FormMessageVariants({ state }), className)}
         {...others}
         ref={ref}
       />
