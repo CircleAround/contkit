41342cd2bb438d90fcfa2d99c937a996cfa85ae4
hiokky <hiokky.work@gmail.com>
2024-10-30 21:00:33 +0900

    チェック時にエラーメッセージを非表示

--- site/src/components/Form/Checkbox/Checkbox.tsx
+++ site/src/components/Form/Checkbox/Checkbox.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { ComponentProps, forwardRef } from 'react';
+import React, { ComponentProps, forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { Checkbox as UICheckbox } from "@/components/ui/checkbox"
+import { Checkbox as UICheckbox } from "@/components/ui/checkbox";
 
 type CheckboxProps = ComponentProps<typeof UICheckbox> & {
   caption?: string;
-}
+  id?: string;
+  checked?: boolean;
+  onCheckedChange?: (checked: boolean) => void;
+};
 
-const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(({ className, caption, ...others }, ref) => {
+const Checkbox = forwardRef<HTMLButtonElement, CheckboxProps>(({ className, caption, id, checked, onCheckedChange, ...others }, ref) => {
   const baseCn = 'border-zinc-400 w-5 h-5 data-[state=checked]:bg-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-600 focus:ring-offset-0 focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-0';
 
-    return (
-      <label className="flex cursor-pointer items-center space-x-2">
-        <UICheckbox ref={ref} className={twMerge(baseCn, className)} {...others} />
-        {caption &&
-          <span className="text-sm text-zinc-900">{caption}</span>
-        }
-      </label>
-    )
-  },
-)
+  return (
+    <label htmlFor={id} className="flex cursor-pointer items-center space-x-2">
+      <UICheckbox
+        id={id}
+        ref={ref}
+        className={twMerge(baseCn, className)}
+        aria-checked={checked}
+        onClick={() => onCheckedChange && onCheckedChange(!checked)}
+        {...others}
+      />
+      {caption && <span className="text-sm text-zinc-900">{caption}</span>}
+    </label>
+  );
+});
 
-Checkbox.displayName = 'Checkbox'
+Checkbox.displayName = 'Checkbox';
 
-export { Checkbox }
+export { Checkbox };
