28ceedf4980c61d10b9142ec8d5e3e97752d11f2
hiokky <hiokky.work@gmail.com>
2024-11-01 16:00:35 +0900

    withRHFデコレーターを読み込みFormProviderでラップする

--- site/src/components/Form/FormMessage/FormMessage.stories.tsx
+++ site/src/components/Form/FormMessage/FormMessage.stories.tsx
@@ -1,8 +1,42 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
+import { ReactNode, FC } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import { FormMessage } from './FormMessage';
 
+// FormProviderラッパーを提供する関数
+function StorybookFormProvider({
+  children,
+}: {
+  children: ReactNode
+}): JSX.Element {
+  const methods = useForm()
+  return (
+    <FormProvider {...methods}>
+      <form
+        onSubmit={methods.handleSubmit(action('[React Hooks Form] Submit'))}
+      >
+        {children}
+      </form>
+    </FormProvider>
+  )
+}
+
+// withRHFデコレーター関数
+function withRHF(showSubmitButton: boolean) {
+  return function (Story: FC) {
+    return (
+      <StorybookFormProvider>
+        <Story />
+        {showSubmitButton && <button type="submit">Submit</button>}
+      </StorybookFormProvider>
+    );
+  };
+}
+
 const meta: Meta<typeof FormMessage> = {
-  title: 'Components/FormMessage',
+  title: 'Components/Form/FormMessage',
   component: FormMessage,
   argTypes: {
     state: {
@@ -16,6 +50,7 @@ const meta: Meta<typeof FormMessage> = {
       description: '表示するメッセージ内容',
     },
   },
+  decorators: [withRHF(false)],
 };
 
 export default meta;
