5cbfaf7381a251006b1f13339ddc07503c6f2e76
hiokky <hiokky.work@gmail.com>
2024-11-01 12:40:00 +0900

    Checkboxコンポーネントをstyleの指定のみに修正

--- site/src/components/Form/Checkbox/Checkbox.stories.ts
+++ site/src/components/Form/Checkbox/Checkbox.stories.ts
@@ -5,19 +5,9 @@ const meta: Meta<typeof Checkbox> = {
   title: 'Components/Form/Checkbox',
   component: Checkbox,
   argTypes: {
-    caption: {
-      control: 'text',
-      description: 'Checkbox のキャプションテキスト',
-      defaultValue: 'Agree to terms',
-    },
-    className: {
-      control: 'text',
-      description: 'Checkbox のカスタムクラス名',
-    },
     checked: {
       control: 'boolean',
-      description: 'Checkbox のチェック状態',
-      defaultValue: false,
+      description: 'チェックボックスの選択状態',
     },
   },
 };
@@ -28,13 +18,6 @@ type Story = StoryObj<typeof Checkbox>;
 
 export const Default: Story = {
   args: {
-    caption: 'I agree to the terms and conditions',
-  },
-};
-
-export const Checked: Story = {
-  args: {
-    caption: 'I agree to the terms and conditions',
-    checked: true,
+    checked: false,
   },
 };
