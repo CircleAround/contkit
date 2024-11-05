65a5b3c495f673e0491a858ab351956705d98b00
hiokky <hiokky.work@gmail.com>
2024-11-05 12:52:55 +0900

    アクセシビリティ関連の修正

--- site/src/components/Form/Textarea/Textarea.tsx
+++ site/src/components/Form/Textarea/Textarea.tsx
@@ -6,7 +6,7 @@ import { Textarea as UITextarea } from '@/components/ui/textarea';
 type TextareaProps = ComponentProps<typeof UITextarea>
 
 const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(({className, children, ...others }, ref) => {
-  const baseCn = 'min-h-36 border-zinc-300 text-zinc-900 focus-visible:ring-1 focus-visible:ring-blue-600 focus-visible:ring-offset-0 placeholder:text-zinc-400'
+  const baseCn = 'min-h-36 border-zinc-300 text-zinc-900 focus-visible:ring-2 focus-visible:ring-blue-600 focus-visible:ring-offset-2 placeholder:text-zinc-400'
 
     return (
       <UITextarea
