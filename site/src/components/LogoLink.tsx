2cb618c843d62b6fa096399b8ec02fbf2a2bc623
hiokky <hiokky.work@gmail.com>
2024-11-08 16:09:56 +0900

    childrenの中身に応じてクラスを変化

--- site/src/components/LogoLink.tsx
+++ site/src/components/LogoLink.tsx
@@ -1,40 +1,26 @@
-import React from 'react';
 import { forwardRef, ComponentProps } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { cva, VariantProps } from 'class-variance-authority';
-
-const LogoLinkVariants = cva(
-  'transition duration-300 ease-in-out hover:opacity-50',
-  {
-    variants: {
-      variant: {
-        text: "text-2xl font-bold text-zinc-900 lg:text-3xl",
-        image: "aspect-video w-20",
-      },
-    },
-    defaultVariants: {
-      variant: "text",
-    },
-  }
-)
 
 type LogoLinkProps = ComponentProps<'a'> & {
-  text?: string;
-  imgComponent?: React.ReactNode;
-} & VariantProps<typeof LogoLinkVariants>;
+  children?: React.ReactNode;
+}
+
+const baseCn = 'transition duration-300 ease-in-out hover:opacity-50'
+const textCn = 'text-2xl font-bold text-zinc-900 lg:text-3xl'
+const imageCn ='aspect-video w-20'
 
 const LogoLink = forwardRef<HTMLAnchorElement, LogoLinkProps>(
-  ({ variant, text, imgComponent, href, className, ...others }, ref) => {
+  ({ href, className, children, ...others }, ref) => {
+    const contentCn = typeof (children)  === 'string' ? textCn : imageCn;
+
     return (
       <a
-        href="/"
-        aria-label="ホームへ戻る"
-        className={twMerge(LogoLinkVariants({ variant }), className)}
+        href={href}
+        className={twMerge(baseCn, contentCn, className)}
         {...others}
         ref={ref}
       >
-        {variant === 'text' && text}
-        {variant === 'image' && imgComponent }
+        {children}
       </a>
     );
   }
