f0bf06889a01c3d5848d1ac586fc28ac74f93e74
hiokky <hiokky.work@gmail.com>
2024-11-08 12:51:49 +0900

    Footerを必要最小限の構成に修正

--- site/src/components/Footer/Footer.tsx
+++ site/src/components/Footer/Footer.tsx
@@ -1,62 +1,24 @@
 import React from 'react';
 import { forwardRef, ComponentProps } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { Navigation } from '../Navigation/Navigation';
-import SectionInner from '../SectionInner';
-import { LogoLink } from '../LogoLink';
-
-type NavLink = {
-  label: string;
-  href: string;
-};
 
 type FooterProps = ComponentProps<'footer'> & {
-  logoLinkVariant: 'text' | 'image';
-  logoText?: string;
-  logoImgSrc?: string;
-  logoimgAlt?: string;
-  navLinks: NavLink[];
-  copyright?: string;
+  children?: React.ReactNode;
 };
 
-const Footer = forwardRef<HTMLElement, FooterProps>(
-  ({ logoLinkVariant, logoText, logoImgSrc, logoimgAlt, navLinks, className, copyright, ...others }, ref) => {
-    const baseCn = 'py-10 bg-zinc-900 text-zinc-400';
+const Footer = forwardRef<HTMLElement, FooterProps>(({ className, children, ...others }, ref) => {
+  const baseCn = 'py-10 bg-zinc-900';
 
-    return (
-      <footer
-        className={twMerge(baseCn, className)}
-        {...others}
-        ref={ref}
-      >
-        <SectionInner>
-          <div className="space-y-6">
-            <div className="flex flex-col justify-between space-y-6 md:flex-row md:space-y-0">
-              <LogoLink
-                variant={logoLinkVariant}
-                text={logoText}
-                imgSrc={logoImgSrc}
-                imgAlt={logoimgAlt}
-                className="text-zinc-400"
-              />
-              <Navigation
-                variant="footer"
-                navLinks={navLinks}
-                className='[&_li]:text-xl [&_li]:md:text-sm [&_ul]:flex [&_ul]:flex-col [&_ul]:items-start [&_ul]:space-x-0 [&_ul]:space-y-4 [&_ul]:md:flex-row [&_ul]:md:space-x-4 [&_ul]:md:space-y-0'
-              />
-            </div>
-
-            <hr className='border border-zinc-700' />
-
-            <div className="mt-6 text-center">
-              <small>{copyright}</small>
-            </div>
-          </div>
-        </SectionInner>
-      </footer>
-    );
-  }
-);
+  return (
+    <footer
+      className={twMerge(baseCn, className)}
+      {...others}
+      ref={ref}
+    >
+      {children}
+    </footer>
+  );
+});
 
 Footer.displayName = 'Footer';
 
