f25a6ebb3348bceb7638117cb50d8346161d5572
hiokky <hiokky.work@gmail.com>
2024-11-08 14:18:02 +0900

    Footerの変更に伴う記述修正

--- site/src/components/layout.tsx
+++ site/src/components/layout.tsx
@@ -1,6 +1,9 @@
 import { FC, PropsWithChildren } from 'react';
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
+import  SectionInner from '../components/SectionInner';
+import { LogoLink } from '../components/LogoLink';
+import { Navigation } from '../components/Navigation/Navigation';
 
 const navLinks = [
   { label: 'About', href: '/' },
@@ -13,9 +16,6 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <Header
-        logoLinkVariant="text"
-        logoText='Logo'
-        logoImgSrc="https://placehold.jp/30/333333/ffffff/300x150.png?text=logo+image"
         navLinks={navLinks}
         ctaChildren="お問い合わせ"
       />
@@ -24,13 +24,25 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
         {children}
       </main>
 
-      <Footer
-        logoLinkVariant="image"
-        logoImgSrc="https://placehold.jp/30/333333/ffffff/300x150.png?text=logo+image"
-        logoimgAlt="logo image"
-        navLinks={navLinks}
-        copyright="© 2024 CIRCLE AROUND Inc."
-      />
+      <Footer>
+        <SectionInner>
+          <div className="flex flex-col justify-between space-y-6 md:flex-row md:space-y-0">
+            <LogoLink
+              variant="image"
+              imgComponent={<img src="https://placehold.jp/30/333333/ffffff/300x150.png?text=logo+image" alt=" image" className='h-auto w-full object-cover object-center' />}
+            />
+            <Navigation
+              variant="footer"
+              navLinks={navLinks}
+              className='text-zinc-400 [&_li]:text-xl [&_li]:md:text-sm [&_ul]:flex [&_ul]:flex-col [&_ul]:items-start [&_ul]:space-x-0 [&_ul]:space-y-4 [&_ul]:md:flex-row [&_ul]:md:space-x-4 [&_ul]:md:space-y-0'
+            />
+          </div>
+
+            <div className="mt-6 text-center">
+              <small className='text-zinc-400'>© 2024 CIRCLE AROUND Inc.</small>
+            </div>
+        </SectionInner>
+      </Footer>
     </>
   );
 };
