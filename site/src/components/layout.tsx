df33599ac788410063237d1f0de073c6b32c118a
hiokky <hiokky.work@gmail.com>
2024-11-08 14:39:35 +0900

    不要なFooterのstories消去に伴う修正

--- site/src/components/layout.tsx
+++ site/src/components/layout.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren } from 'react';
 import { Header } from '../components/Header/Header';
-import { Footer } from '../components/Footer/Footer';
+import { Footer } from './Footer';
 import  SectionInner from '../components/SectionInner';
 import { LogoLink } from '../components/LogoLink';
 import { Navigation } from '../components/Navigation/Navigation';
