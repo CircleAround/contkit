40c7aa2f76bc4df7bf014a3c5e3a2f807a76e1bb
hiokky <hiokky.work@gmail.com>
2024-11-13 11:55:00 +0900

    Navigationのコンポーネント分割とクラス上書き対応

--- site/src/components/Navigation/Navigation.tsx
+++ site/src/components/Navigation/Navigation.tsx
@@ -1,48 +1,95 @@
 import { forwardRef, ComponentProps } from 'react';
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
+  NavigationMenu as UINavigationMenu,
+  NavigationMenuList as UINavigationMenuList,
+  NavigationMenuItem as UINavigationMenuItem,
+  NavigationMenuLink as UINavigationMenuLink,
 } from '@/components/ui/navigation-menu';
 import { twMerge } from 'tailwind-merge';
-import { Link } from 'gatsby';
 
-type NavLink = {
-  label: string;
-  href: string;
-};
-
-type NavigationMenuProps = ComponentProps<typeof NavigationMenu> & {
-  navLinks: NavLink[];
-}
+// Navigation
+type NavigationMenuProps = ComponentProps<typeof UINavigationMenu>
 
 const Navigation = forwardRef<HTMLElement, NavigationMenuProps>(
-  ({ navLinks, className, ...others }, ref) => {
+  ({ className, children, ...others }, ref) => {
     const baseCn = 'text-sm font-semibold';
 
     return (
-      <NavigationMenu
+      <UINavigationMenu
         className={twMerge(baseCn, className)}
         {...others}
         ref={ref}
       >
-        <NavigationMenuList className="flex justify-start space-x-4">
-          {navLinks.map((navLink) => (
-            <NavigationMenuItem key={navLink.label}>
-              <Link
-                to={navLink.href}
-                className="relative pb-1 transition duration-1000 ease-in-out after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 after:content-[''] hover:after:w-full"
-              >
-                {navLink.label}
-              </Link>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
+        {children}
+      </UINavigationMenu>
+    );
+  }
+);
+
+// NavigationMenuList
+type NavigationMenuListProps = ComponentProps<typeof UINavigationMenuList>
+
+const NavigationMenuList = forwardRef<HTMLUListElement, NavigationMenuListProps>(
+  ({ className, children, ...others }, ref) => {
+    const baseCn = 'flex flex-col items-start space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0';
+
+    return (
+      <UINavigationMenuList
+        className={twMerge(baseCn, className)}
+        {...others}
+        ref={ref}
+      >
+        {children}
+      </UINavigationMenuList>
+    );
+  }
+);
+
+// NavigationMenuItem
+type NavigationMenuItemProps = ComponentProps<typeof UINavigationMenuItem>
+
+const NavigationMenuItem = forwardRef<HTMLLIElement, NavigationMenuItemProps>(
+  ({ className, children, ...others }, ref) => {
+    return (
+      <UINavigationMenuItem
+        className={className}
+        {...others}
+        ref={ref}
+      >
+        {children}
+      </UINavigationMenuItem>
+    );
+  }
+);
+
+// NavigationLink
+type NavigationLinkProps = ComponentProps<typeof UINavigationMenuLink> & {
+  href: string;
+}
+
+const NavigationLink = forwardRef<HTMLAnchorElement, NavigationLinkProps>(
+  ({ href, className, children, ...others }, ref) => {
+    const baseCn ='text-zinc-900 text-sm'
+    const hoverAnimationCn = 'transition duration-300 ease-in-out';
+    const focusCn = 'focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-600 focus-visible:ring-offset-2';
+
+    return (
+      <UINavigationMenuLink
+        href={href}
+        className={twMerge(baseCn, hoverAnimationCn, focusCn, className)}
+        {...others}
+        ref={ref}
+      >
+        {children}
+      </UINavigationMenuLink>
     );
   }
 );
 
 Navigation.displayName = 'Navigation';
+NavigationMenuList.displayName = 'NavigationMenuList';
+NavigationMenuItem.displayName = 'NavigationMenuItem';
+NavigationLink.displayName = 'NavigationLink';
 
-export { Navigation };
+export { Navigation, NavigationMenuList, NavigationMenuItem, NavigationLink };
+
