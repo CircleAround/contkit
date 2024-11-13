0106b1595c86a79396185a32ac184ad822b7d7ef
hiokky <hiokky.work@gmail.com>
2024-11-13 17:50:30 +0900

    Headerコンポーネントの修正

--- site/src/components/Header/Header.tsx
+++ site/src/components/Header/Header.tsx
@@ -1,111 +1,50 @@
 import React from 'react';
-import { AnchorButton } from '../Button/Button';
-import { ReactNode, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { Navigation } from '../Navigation/Navigation';
-import { LogoLink } from '../LogoLink';
+import { forwardRef, ComponentProps } from 'react';
 
-type NavLink = {
-  label: string;
-  href: string;
-};
-
-const Header = ({
-  imgComponent,
-  href,
-  ctaChildren,
-  navLinks,
-  navClassName,
-  className
-}: {
-  imgComponent: ReactNode,
-  href: string,
-  ctaChildren: ReactNode,
-  navLinks: NavLink[],
-  navClassName?: string,
+// Header
+type HeaderProps = ComponentProps<'header'> & {
   className?: string
-}) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+}
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+const Header = forwardRef<HTMLElement, HeaderProps>(
+  ({ className, children, ...others }, ref) => {
+    const baseCn = 'fixed top-6 left-0 z-40 px-6 w-full md:px-10';
 
-  return (
-    <HeaderLayout className={className}>
-      <LogoLink href={href}>{imgComponent}</LogoLink>
-      <Navigation navLinks={navLinks} className={navClassName} />
-      <HeaderButton className={className} ctaChildren={ctaChildren} />
-      <HamburgerIcon isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} className={className} />
-      <MobileMenu navLinks={navLinks} isMenuOpen={isMenuOpen} ctaChildren={ctaChildren} className={className} />
-    </HeaderLayout>
-  );
-};
+    return (
+      <header
+        className={twMerge(baseCn, className)}
+        {...others}
+        ref={ref}
+      >
+        {children}
+      </header>
+    );
+  }
+);
 
-const HeaderLayout = ({ children, className }: { children: ReactNode, className?: string }) => {
-  return (
-    <header className={twMerge('fixed top-6 left-0 z-40 px-6 w-full md:px-10', className)}>
-      <div className="flex items-center justify-between">
+// HeaderInner
+type HeaderInnerProps = ComponentProps<'header'> & {
+  className?: string
+}
+
+const HeaderInner = forwardRef<HTMLDivElement, HeaderInnerProps>(
+  ({ className, children, ...others }, ref) => {
+    const baseCn = 'flex items-center justify-between';
+
+    return (
+      <div
+        className={twMerge(baseCn, className)}
+        {...others}
+        ref={ref}
+      >
         {children}
       </div>
-    </header>
-  );
-};
-
-const HeaderButton = ({ className, ctaChildren }: { className?: string, ctaChildren: ReactNode }) => {
-  return (
-    <div className={twMerge("hidden md:block", className)}>
-      <AnchorButton variant="primary" href={''}>{ctaChildren}</AnchorButton>
-    </div>
-  );
-};
-
-const HamburgerIcon = ({ isMenuOpen, toggleMenu, className }: { isMenuOpen: boolean, toggleMenu: () => void, className?: string }) => {
-  return (
-    <div className={twMerge("z-50 md:hidden", className)}>
-      <button
-        type="button"
-        onClick={toggleMenu}
-        aria-label={isMenuOpen ? "メニューを閉じる" : "メニューを開く"}
-        className={twMerge(
-          "flex size-12 flex-col items-center justify-center rounded-full bg-zinc-900",
-          isMenuOpen ? 'justify-center' : '',
-          className
-        )}
-      >
-        <div className={`h-1 w-6 bg-white transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-y-2 rotate-45' : ''}`}></div>
-        <div className={`my-1 h-1 w-6 bg-white transition-opacity duration-300 ease-in-out ${isMenuOpen ? 'opacity-0' : ''}`}></div>
-        <div className={`h-1 w-6 bg-white transition-transform duration-300 ease-in-out ${isMenuOpen ? '-translate-y-2 -rotate-45' : ''}`}></div>
-      </button>
-    </div>
-  );
-};
-
-const MobileMenu = ({ navLinks, isMenuOpen, ctaChildren, className }: { navLinks: NavLink[], isMenuOpen: boolean, ctaChildren: ReactNode, className?: string }) => {
-  return (
-    <>
-      <div
-        className={twMerge(
-          'fixed left-0 top-0 flex w-full h-screen flex-col justify-start bg-zinc-100 px-6 md:hidden transition-all duration-500 ease-in-out',
-          isMenuOpen ? '-translate-x-0 opacity-100' : 'translate-x-full opacity-0',
-          className
-        )}
-      >
-        <div className="pb-6 pt-24">
-          <Navigation navLinks={navLinks} className='flex md:hidden [&_li]:text-xl [&_ul]:flex-col [&_ul]:items-start [&_ul]:space-x-0 [&_ul]:space-y-4'/>
-          <div className="mt-6">
-            <HeaderButton ctaChildren={ctaChildren} className='block'/>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+    );
+  }
+);
 
 Header.displayName = 'Header';
-HeaderLayout.displayName = 'HeaderLayout';
-HeaderButton.displayName = 'HeaderButton';
-HamburgerIcon.displayName = 'HamburgerIcon';
-MobileMenu.displayName = 'MobileMenu';
+HeaderInner.displayName = 'HeaderInner';
 
-export { Header, HeaderLayout, HeaderButton, HamburgerIcon, MobileMenu };
+export { Header, HeaderInner };
