06dd61b9a5927bbf68e2af0d38ec1109c4cbc52f
hiokky <hiokky.work@gmail.com>
2024-11-19 11:52:16 +0900

    各ページにSeoコンポーネントを設置

--- site/src/components/Seo.tsx
+++ site/src/components/Seo.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Seo = ({ children } : { children?: React.ReactNode }) => (
+const Seo = ({ title, children } : { title: string, children?: React.ReactNode }) => (
+  
   <>
-    <title>Tech lib 〜 WEBプログラミング基礎の動画解説をここに集約 〜</title>
+    <title>{title}</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="WEBプログラミングの基礎を動画でわかりやすく解説してここに集約します。" />
@@ -12,6 +13,7 @@ const Seo = ({ children } : { children?: React.ReactNode }) => (
     <meta property="og:type" content="website" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@CircleAroundCo" />
+    <meta name="twitter:title" content="Tech lib 〜 WEBプログラミング基礎の動画解説をここに集約 〜" />
     {children}
   </>
 );
