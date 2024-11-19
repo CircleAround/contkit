c4bcf08bcc07cc61be94550ba84eb7a42badb85c
hiokky <hiokky.work@gmail.com>
2024-11-19 10:59:58 +0900

    Seoコンポーネント追加

---
+++ site/src/components/Seo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Seo = ({ children } : { children?: React.ReactNode }) => (
+  <>
+    <title>Tech lib 〜 WEBプログラミング基礎の動画解説をここに集約 〜</title>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="WEBプログラミングの基礎を動画でわかりやすく解説してここに集約します。" />
+    <meta property="og:title" content="Tech lib 〜 WEBプログラミング基礎の動画解説をここに集約 〜" />
+    <meta property="og:description" content="WEBプログラミングの基礎を動画でわかりやすく解説してここに集約します。" />
+    <meta property="og:image" content="https://techlib.circlearound.co.jp/images/ogp_image.jpg" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@CircleAroundCo" />
+    {children}
+  </>
+);
+
+Seo.displayName = 'Seo';
+
+export { Seo };
