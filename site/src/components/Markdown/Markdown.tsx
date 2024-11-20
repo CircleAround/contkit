7f9af59dfeb0ba8fd3e924a9aa9993a3898127aa
Masashi SATO <ms2.sato@gmail.com>
2024-11-20 13:40:04 +0900

    add remark-gfm for table on Markdwon

--- site/src/components/Markdown/Markdown.tsx
+++ site/src/components/Markdown/Markdown.tsx
@@ -1,9 +1,11 @@
 import {remark} from 'remark';
 import remarkHtml from 'remark-html';
+import remarkGfm from 'remark-gfm'
 
 export const Markdown = ({text}: {text: string}) => {
   const htmlContent = remark()
     .use(remarkHtml)
+    .use(remarkGfm)
     .processSync(text)
     .toString();
 
