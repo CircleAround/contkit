f56272ad250543dcfc74e9c411d4f05676c984d3
Masashi SATO <ms2.sato@gmail.com>
2024-11-24 17:44:26 +0900

    add tags for blog

--- site/gatsby-config.ts
+++ site/gatsby-config.ts
@@ -1,24 +1,37 @@
-import type { GatsbyConfig, NodeInput } from 'gatsby'
-import * as admin from 'firebase-admin'
+import type { GatsbyConfig, NodeInput } from "gatsby";
+import * as admin from "firebase-admin";
+
+type Tag = {
+  id: string;
+  name: string;
+  slug: string;
+};
+
+type TagDoc = NodeInput & {
+  name: string;
+  slug: string;
+};
 
 type Blog = {
-  id: string
-  slug: string
-  title: string
-  body: string
-  createdAt: Date
-}
+  id: string;
+  slug: string;
+  title: string;
+  body: string;
+  tags___NODE: string[];
+  createdAt: Date;
+};
 
 type BlogDoc = NodeInput & {
-  body: string
-  slug: string
-  title: string
-  createdAt: admin.firestore.Timestamp
-}
+  body: string;
+  slug: string;
+  title: string;
+  tagIds: string[];
+  createdAt: admin.firestore.Timestamp;
+};
 
 const config: GatsbyConfig = {
   // @see https://www.gatsbyjs.com/docs/reference/release-notes/v4.1/#jsx-runtime-options-in-gatsby-configjs
-  jsxRuntime: 'automatic',
+  jsxRuntime: "automatic",
   siteMetadata: {
     title: `Starter gatsby firebase site`,
     siteUrl: `https://www.yourdomain.tld`,
@@ -28,50 +41,60 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    'gatsby-plugin-image',
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-plugin-google-gtag',
+      resolve: "gatsby-plugin-google-gtag",
       options: {
         trackingIds: [],
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        icon: 'src/images/icon.png',
+        icon: "src/images/icon.png",
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'images',
-        path: './src/images/',
+        name: "images",
+        path: "./src/images/",
       },
-      __key: 'images',
+      __key: "images",
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'pages',
-        path: './src/pages/',
+        name: "pages",
+        path: "./src/pages/",
       },
-      __key: 'pages',
+      __key: "pages",
     },
     {
-      resolve: 'gatsby-firesource',
+      resolve: "gatsby-firesource",
       options: {
         types: [
           {
-            type: 'Blog',
-            collection: 'blogs',
+            type: "Tag",
+            collection: "tags",
+            map: (doc: TagDoc): Tag => ({
+              id: doc.id,
+              name: doc.name,
+              slug: doc.slug,
+            }),
+          },
+          {
+            type: "Blog",
+            collection: "blogs",
             map: (doc: BlogDoc): Blog => ({
               id: doc.id,
               slug: doc.slug,
               title: doc.title,
               body: doc.body,
+              tags___NODE: doc.tagIds || [],
               createdAt: doc.createdAt.toDate(),
             }),
           },
@@ -79,6 +102,6 @@ const config: GatsbyConfig = {
       },
     },
   ],
-}
+};
 
-export default config
+export default config;
