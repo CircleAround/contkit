9a73274cbdb137a2335d1c90a36a1349fa3cef41
hiokky <hiokky.work@gmail.com>
2024-11-25 18:07:45 +0900

    sidebarを別コンポーネント化

--- site/src/pages/entries.tsx
+++ site/src/pages/entries.tsx
@@ -1,10 +1,9 @@
 import { HeadFC, PageProps, graphql } from 'gatsby'
 import Layout from '@/components/layout'
-import { BadgeList } from '@/components/Badge/BadgeList'
-import { Badge } from '@/components/Badge/Badge'
 import { SectionTitle } from '@/components/SectionTitle/SectionTitle'
 import { Breadcrumb } from '@/components/Breadcrumb/Breadcrumb'
 import { Seo } from '@/components/Seo'
+import { Sidebar } from '@/components/Sidebar'
 
 // タグ一覧確認ようのための仮
 const tags =[{ label: 'データベース', link: '/tags' }, { label: 'セッション', link: '/tags' }, { label: '開発者ツール', link: '/tags' }, { label: '実演', link: '/tags' }];
@@ -46,34 +45,7 @@ const BlogIndexPage: React.FC<PageProps<Queries.BlogIndexQuery>> = ({ data }) =>
             </ul>
           </section>
 
-          <aside className="md:col-span-3 md:pl-4">
-            <SectionTitle
-              className="text-3xl font-medium leading-relaxed text-blue-950	 lg:text-3xl lg:leading-relaxed"
-            >
-              タグ一覧
-            </SectionTitle>
-            {/* タグ一覧 ※後日実装のため仮置き */}
-            <ul className="mt-4">
-              <BadgeList className="md:flex-col md:gap-y-2.5">
-                {tags?.map((tag) => (
-                  <li
-                    key={tag.label}
-                    className="md:border-b md:border-palePurple-600 md:pb-1"
-                  >
-                    <a href={tag.link}>
-                      <Badge
-                        variant="primary"
-                        shape="sm"
-                        className="bg-palePurple-600 py-0.5 text-[10px] md:bg-transparent md:px-0 md:text-base md:font-bold md:text-palePurple-600"
-                      >
-                        {tag.label}
-                      </Badge>
-                    </a>
-                  </li>
-                ))}
-              </BadgeList>
-            </ul>
-          </aside>
+          <Sidebar className='md:col-span-3 md:pl-4'/>
         </div>
       </div>
     </Layout>
