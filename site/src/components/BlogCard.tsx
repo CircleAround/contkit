f28278229d4d49e266481232c1d251f74d599352
hiokky <hiokky.work@gmail.com>
2024-11-27 11:41:27 +0900

    一覧で使い回すカードを別コンポーネント化

---
+++ site/src/components/BlogCard.tsx
@@ -0,0 +1,64 @@
+import { forwardRef, ComponentProps } from 'react';
+import { Card, CardImage, CardContent, CardTitle } from '@/components/Card/Card'
+import { BadgeList } from '@/components/Badge/BadgeList'
+import { Badge } from '@/components/Badge/Badge'
+import { Link } from 'gatsby';
+
+type BlogCardProps = ComponentProps<typeof Card> & {
+  blogUrl: string;
+  title: string;
+  imgsrc: string;
+  publishedAtStr: string;
+  tags?: {
+    label: string;
+    link: string;
+  }[]
+  className?: string;
+}
+
+const BlogCard = forwardRef<HTMLDivElement, BlogCardProps>(({ blogUrl, title, imgsrc, publishedAtStr, tags, className, ...others }, ref) => {
+
+  return (
+    <Card
+      style="none"
+      size="none"
+      className={className}
+      {...others}
+      ref={ref}
+    >
+      <Link to={blogUrl}>
+        <CardImage
+          imgSrc={imgsrc}
+          imgAlt=''
+          className="rounded-none"
+        />
+      </Link>
+      <CardContent className='bg-gray-50 p-4'>
+        {tags &&  (
+          <BadgeList>
+            {tags?.map((tag) => (
+              <li key={tag.label}>
+                <Link to={tag.link}>
+                  <Badge
+                    shape="sm"
+                    className="bg-palePurple-600 py-0.5 text-[10px]"
+                  >
+                    {tag.label}
+                  </Badge>
+                </Link>
+              </li>
+            ))}
+          </BadgeList>
+        )}
+        <Link to={blogUrl}>
+          <CardTitle>{title}</CardTitle>
+        </Link>
+        <small className="mt-auto text-zinc-600">{publishedAtStr}</small>
+      </CardContent>
+    </Card>
+  );
+});
+
+BlogCard.displayName = 'BlogCard';
+
+export { BlogCard };
