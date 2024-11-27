33caae10cf3c84f4c75bfa8315af801e9ab7b2d3
hiokky <hiokky.work@gmail.com>
2024-11-27 11:41:27 +0900

    一覧で使い回すカードを別コンポーネント化

--- site/src/pages/entries.tsx
+++ site/src/pages/entries.tsx
@@ -1,26 +1,25 @@
-import { HeadFC, Link, PageProps, graphql } from 'gatsby'
+import { HeadFC, PageProps, graphql } from 'gatsby'
 import Layout from '@/components/layout'
 import { SectionTitle } from '@/components/SectionTitle/SectionTitle'
 import { Breadcrumb } from '@/components/Breadcrumb/Breadcrumb'
 import { Seo } from '@/components/Seo'
 import { Sidebar } from '@/components/Sidebar'
-import { Card, CardImage, CardContent, CardTitle } from '@/components/Card/Card'
-import { BadgeList } from '@/components/Badge/BadgeList'
-import { Badge } from '@/components/Badge/Badge'
+import { BlogCard } from '@/components/BlogCard'
 
-
-const BlogIndexPage: React.FC<PageProps<Queries.BlogIndexQuery>> = ({ data }) => {
-
+const BlogIndexPage: React.FC<PageProps<Queries.BlogIndexQuery>> = ({ data: { allBlog, site} }) => {
+  if(!allBlog) {
+    throw new Error('post is required')
+  }
   // ダミー
   const imgsrc = 'https://techlib.circlearound.co.jp/static/28ba383fd275be0db126f951f18eae15/73f08/rest-history-and-foundation-knowledge.png'
   const tags = [
     { label: 'React', link: '/tags/react' },
     { label: 'Gatsby', link: '/tags/gatsby' },
   ]
+  const blogUrl = '/entries/1'
 
   return (
     <Layout>
-      <h2>{data.site?.siteMetadata?.title}</h2>
 
       <div className="py-8">
         <div className="grid grid-cols-1 gap-8 px-8 md:grid-cols-12 md:divide-x md:divide-gray-300">
@@ -39,40 +38,15 @@ const BlogIndexPage: React.FC<PageProps<Queries.BlogIndexQuery>> = ({ data }) =>
             <div className="mt-12"></div>
 
             <ul className="mt-8 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-[5vmin]">
-              {data.allBlog.edges.map(({ node: { id, title, slug, createdAt } }) => (
+              {allBlog.edges.map(({ node: { id, title, createdAt } }) => (
                 <li key={id}>
-                  <Card
-                    style="none"
-                    size="none"
-                  >
-                    <Link to={`/entries/${slug}`}>
-                      <CardImage
-                        imgSrc={imgsrc}
-                        imgAlt=''
-                        className="rounded-none"
-                      />
-                    </Link>
-                    <CardContent className='bg-gray-50 p-4'>
-                      <BadgeList>
-                        {tags?.map((tag) => (
-                          <li key={tag.label}>
-                            <Link to={tag.link}>
-                              <Badge
-                                shape="sm"
-                                className="bg-palePurple-600 py-0.5 text-[10px]"
-                              >
-                                {tag.label}
-                              </Badge>
-                            </Link>
-                          </li>
-                        ))}
-                      </BadgeList>
-                      <Link to={`/entries/${slug}`}>
-                        <CardTitle>{title}</CardTitle>
-                      </Link>
-                      <small className="mt-auto text-zinc-600">{createdAt}</small>
-                    </CardContent>
-                  </Card>
+                  <BlogCard
+                    blogUrl={blogUrl}
+                    title={title}
+                    imgsrc={imgsrc}
+                    publishedAtStr={createdAt}
+                    tags={tags}
+                  />
                 </li>
               ))}
             </ul>
