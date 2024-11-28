7a1bb78246fd509f9f986a3b24318b2e21d13647
hiokky <hiokky.work@gmail.com>
2024-11-28 15:12:19 +0900

    不要な記述の消去

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -2,54 +2,10 @@ import { Link, type HeadFC, type PageProps, graphql } from "gatsby"
 import moment from 'moment'
 import { SectionTitle } from '../components/SectionTitle/SectionTitle'
 import  Layout  from '../components/layout'
-import {
-  Card,
-  CardImage,
-  CardContent,
-  CardTitle,
-} from '../components/Card/Card';
-import { Badge } from '../components/Badge/Badge';
-import { BadgeList } from '@/components/Badge/BadgeList';
 import { Sidebar } from '@/components/Sidebar';
 import { BlogCard } from '@/components/BlogCard';
 import { Seo } from '@/components/Seo';
 import heroBg from "../images/hero_bg.jpg"
-import ogpBg from "../images/ogp_bg.jpg"
-
-const contents = [
-  {
-    link: '/',
-    imgSrc: ogpBg,
-    imgAlt: 'ざっくりRedis入門 セッション情報を管理する',
-    date: '2022/02/05 11:30',
-    title: 'ざっくりRedis入門 セッション情報を管理する',
-    badge: [{ label: 'データベース', link: '/tags' }, { label: 'セッション', link: '/tags' }, { label: '開発者ツール', link: '/tags' }, { label: '実演', link: '/tags' }],
-  },
-  {
-    link: '/blogs/test1',
-    imgSrc: ogpBg,
-    imgAlt: 'HTTPレスポンス',
-    date: '2022/01/19 09:00',
-    title: 'HTTPレスポンス',
-    badge: [{ label: '基礎', link: '/tags' }, { label: 'Webシステム', link: '/tags' }],
-  },
-  {
-    link: '/blogs/test1',
-    imgSrc: ogpBg,
-    imgAlt: 'Babelの入門',
-    date: '2022/01/11 15:00',
-    title: 'Babelの入門',
-    badge: [{ label: 'JavaScript', link: '/tags' }, { label: '実演', link: '/tags' }, { label: 'ステップアップJavaScript補足', link: '/tags' }],
-  },
-  {
-    link: '/blogs/test1',
-    imgSrc: ogpBg,
-    imgAlt: 'Node.jsとnpmの入門',
-    date: '2022/01/11 14:30',
-    title: 'Node.jsとnpmの入門',
-    badge: [{ label: 'JavaScript', link: '/tags' }, { label: '実演', link: '/tags' }],
-  },
-];
 
 const IndexPage: React.FC<PageProps<Queries.IndexQuery>> = ({ data: { allBlog } }) => {
   if(!allBlog) {
@@ -59,7 +15,6 @@ const IndexPage: React.FC<PageProps<Queries.IndexQuery>> = ({ data: { allBlog } 
   return (
     <>
       <Layout>
-        {/* heroセクション */}
         <section className="relative">
           <div className="h-[60vh] max-h-[300px] md:max-h-[400px] lg:max-h-[500px]">
             <img
@@ -80,7 +35,6 @@ const IndexPage: React.FC<PageProps<Queries.IndexQuery>> = ({ data: { allBlog } 
 
         <div className="py-8">
           <div className="grid grid-cols-1 gap-8 px-8 md:grid-cols-12 md:divide-x md:divide-gray-300">
-            {/* 新着コンテンツセクション */}
             <section className="md:col-span-9">
               <SectionTitle
                 className="relative border-b border-gray-300 pb-3 pl-8 text-3xl font-medium leading-relaxed text-blue-950	 before:absolute before:left-0
